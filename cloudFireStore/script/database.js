b8a115c0415a9fbd1dc09efcb084c91ca7bb57e7
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-03-29 17:25:38 +0530

    proj 2

--- cloudFireStore/script/database.js
+++ cloudFireStore/script/database.js
@@ -1,11 +1,14 @@
 const submitForm = async() =>{
-    const userName =document.getElementById('userName').value;
+    const dataInDatabase = document.querySelector('.dataInDatabase')
+    const userName =document.querySelector('#userName').value;
     const userEmail =document.querySelector('#userEmail').value;
     const userAge =document.querySelector('#userAge').value;
     const userPhone =document.querySelector('#userPhone').value;
     const userPassword =document.querySelector('#userPassword').value;
     const userCPassword =document.querySelector('#userCPassword').value;
     const error = document.querySelector('.error')
+
+    dataInDatabase.style.display = "block"
 
     if(userPassword != userCPassword){
         error.innerHTML = "Password Do Not Match!!!"
@@ -21,5 +24,23 @@ const submitForm = async() =>{
         } catch (error) {
            console.log(error);
         }
+    }
+}
+
+const getData = async() =>{
+    const userName1 =document.querySelector('#userName1')
+    const userEmail1 =document.querySelector('#userEmail1')
+    const userAge1 =document.querySelector('#userAge1')
+    const userPhone1 =document.querySelector('#userPhone1')
+
+    try {
+        var db = firebase.firestore();
+        const dataPromise = await db.collection("users").doc("eznTlxL0gw19eA1jyFS2").get()
+        userName1.value = dataPromise.data().name;
+        userEmail1.value = dataPromise.data().email;
+        userAge1.value = dataPromise.data().age;
+        userPhone1.value = dataPromise.data().phone;
+    } catch (error) {
+       console.log(error); 
     }
 }
